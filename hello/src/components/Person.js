1c8e45a876e9ab693d09d9e6ae7216c060455bb8
Sridhar Kothapalli <sridr8@gmail.com>
2022-11-03 18:56:24 +0530

    key prop added

--- hello/src/components/Person.js
+++ hello/src/components/Person.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Person({person}) {
   return (
-    <div>Person</div>
+    <div><h2>I'm {person.name} of age {person.age} and know {person.skill}</h2></div>
   )
 }
 
