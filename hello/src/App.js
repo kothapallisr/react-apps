a63c050f7e35e37f222164520b661686da2d0c6a
Sridhar Kothapalli <sridr8@gmail.com>
2022-11-03 20:54:04 +0530

    css module styles

--- hello/src/App.js
+++ hello/src/App.js
@@ -4,10 +4,14 @@ import UserGreeting from './components/UserGreeting';
 import NameList from './components/NameList';
 import Stylesheet from './components/Stylesheet';
 import Inline from './components/Inline';
+import './appStyles.css'
+import styles from './appStyles.module.css'
 
 function App() {
   return (
     <div className="App">
+     <h2 className='error'>Error</h2>
+     <h2 className={styles.success}>Success</h2>
      <Inline/>
      <Stylesheet/>
      <UserGreeting/>
