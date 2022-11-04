9ffe4d4212b258cdcb8cf122875f8fd63b1d00b3
Sridhar Kothapalli <sridr8@gmail.com>
2022-11-04 16:12:16 +0530

    forwarding Refs

--- hello/src/App.js
+++ hello/src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 import ParentComp from './components/ParentComp';
 import RefsDemo from './components/RefsDemo';
 import FocusInput from './components/FocusInput';
+import ForwardRefParentInput from './components/ForwardRefParentInput';
 
 
 function App() {
   return (
     <div className='App'>
-     <FocusInput/>
+     <ForwardRefParentInput/>
     </div>
 
   );
---
+++ hello/src/components/ForwardRefParentInput.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import ForwardRefInput from './ForwardRefInput'
+
+class ForwardRefParentInput extends Component {
+  constructor(props) {
+    super(props)
+    this.inputRef = React.createRef()
+  }
+  handleClick = () => {
+    this.inputRef.current.focus()
+  }
+  render() {
+    return (
+      <div>
+        <ForwardRefInput ref={this.inputRef}/>
+        <button onClick={this.handleClick}>Focus Input</button>
+      </div>
+    )
+  }
+}
+
+export default ForwardRefParentInput
