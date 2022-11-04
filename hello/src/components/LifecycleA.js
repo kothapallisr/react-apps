cfd18382db236a9ad50e778a855d2339fbf814ee
Sridhar Kothapalli <sridr8@gmail.com>
2022-11-04 12:10:04 +0530

    Component updating lifecycle methods

--- hello/src/components/LifecycleA.js
+++ hello/src/components/LifecycleA.js
@@ -8,6 +8,7 @@ class LifecycleA extends Component {
     this.state = {
        name: 'Sridhar'
     }
+    this.changeState = this.changeState.bind(this)
     console.log('LifecycleA constructor')
   }
   static getDerivedStateFromProps(props, state) {
@@ -17,11 +18,31 @@ class LifecycleA extends Component {
   componentDidMount() {
     console.log('LifecycleA componentDidMount')
   }
+  shouldComponentUpdate() {
+    console.log('LifecycleA shouldComponentUpdate')
+    return true
+  }
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log('LifecycleA getSnapshotBeforeUpdate')
+    return null
+  }
+  componentDidUpdate() {
+    console.log('LifecycleA componentDidUpdate')
+  }
+  changeState() {
+    this.setState(
+      {
+        name:'Codevolution'
+      }
+    )
+  }
   render() {
     console.log('LifecycleA render')
     return (
       <div>LifecycleA
+      <p>Name: {this.state.name}</p>
       <LifecycleB/>
+      <button onClick={this.changeState}>Click Me</button>
       </div>
     )
   }
--- hello/src/components/LifecycleB.js
+++ hello/src/components/LifecycleB.js
@@ -16,6 +16,17 @@ class LifecycleB extends Component {
   componentDidMount() {
     console.log('LifecycleB componentDidMount')
   }
+  shouldComponentUpdate() {
+    console.log('LifecycleB shouldComponentUpdate')
+    return true
+  }
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log('LifecycleB getSnapshotBeforeUpdate')
+    return null
+  }
+  componentDidUpdate() {
+    console.log('LifecycleB componentDidUpdate')
+  }
   render() {
     console.log('LifecycleB render')
     return (
