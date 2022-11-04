253035ae23925b8f88bb7f622db98018978e119e
Sridhar Kothapalli <sridr8@gmail.com>
2022-11-04 15:42:19 +0530

    Refs with class components

--- hello/src/App.js
+++ hello/src/App.js
@@ -2,12 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import ParentComp from './components/ParentComp';
 import RefsDemo from './components/RefsDemo';
+import FocusInput from './components/FocusInput';
 
 
 function App() {
   return (
     <div className='App'>
-     <RefsDemo/>
+     <FocusInput/>
     </div>
 
   );
