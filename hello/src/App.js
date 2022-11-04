fc960781bbef0a077509e886904c898e303e5934
Sridhar Kothapalli <sridr8@gmail.com>
2022-11-05 01:30:19 +0530

    HoverCounterTwo

--- hello/src/App.js
+++ hello/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import ClickCounterTwo from './components/ClickCounterTwo';
+import HoverCounterTwo from './components/HoverCounterTwo';
 
 function App() {
   return (
     <div className='App'>
       <ClickCounterTwo/>
+      <HoverCounterTwo/>
     </div>
 
   );
