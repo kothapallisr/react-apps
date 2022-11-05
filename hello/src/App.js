5784e4394ea70ce8563d01f45e2a4c9c9e570cee
Sridhar Kothapalli <sridr8@gmail.com>
2022-11-05 19:57:38 +0530

    passing function as prop

--- hello/src/App.js
+++ hello/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import ClickCounterTwo from './components/ClickCounterTwo';
 import HoverCounterTwo from './components/HoverCounterTwo';
+import User from './components/User';
 
 function App() {
   return (
     <div className='App'>
       <ClickCounterTwo/>
       <HoverCounterTwo/>
+      <User name={() => 'Sridhar'}/>
     </div>
 
   );
