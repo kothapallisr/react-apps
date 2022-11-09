e736579e09d9d525a968a6c677617f6a46a21941
Sridhar Kothapalli <sridr8@gmail.com>
2022-11-09 18:00:16 +0530

    trigger the effect (fetch title) on button click

--- react-hooks/src/components/DataFetching.js
+++ react-hooks/src/components/DataFetching.js
@@ -4,17 +4,22 @@ import axios from 'axios'
 function DataFetching() {
   const [post, setPost] = useState({})
   const [id, setId] = useState(1)
+  const [idFromButtonClick, setIdFromButtonClick] = useState(1)
+  const handleClick = () => {
+    setIdFromButtonClick(id)
+  }
   useEffect(() => {
-    axios.get(`http://jsonplaceholder.typicode.com/posts/${id}`)
+    axios.get(`http://jsonplaceholder.typicode.com/posts/${idFromButtonClick}`)
     .then(res => {
       console.log(res)
       setPost(res.data)
     })
     .catch(error => console.log(error))  
-  },[id]) // [] <- dependency list
+  },[idFromButtonClick]) // [] <- dependency list
   return (
     <div>
       <input type='text' value={id} onChange={e => setId(e.target.value)}/>
+      <button type='button' onClick={handleClick}>Fetch title</button>
       <div>{post.title}</div>
       {/* <ol>
         {posts.map(post => (<li key={post.id}>{post.title}</li>))}
