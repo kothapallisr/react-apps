7abe2221090292000906fd53a082cd5f8a40ad9e
Sridhar Kothapalli <sridr8@gmail.com>
2022-11-09 11:49:16 +0530

    useState for name

--- react-hooks/src/components/HookCounterFive.js
+++ react-hooks/src/components/HookCounterFive.js
@@ -2,11 +2,14 @@ import React, {useState, useEffect} from 'react'
 
 function HookCounterFive() {
   const [count, setCount] = useState(0)
+  const [name, setName] = useState('')
   useEffect(() => {
+    console.log('useEffect - Updating document title')
     document.title = `Clicked ${count} times`
   })
   return (
     <div>
+      <input type='text' value={name} onChange={e => setName(e.target.value)}/>
       <button onClick={() => setCount(count + 1)}>Clicked {count} times</button>
     </div>
   )
