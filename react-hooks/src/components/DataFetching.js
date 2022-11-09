197f6bd79141ee1a70709fe628f6f445f1d8faee
Sridhar Kothapalli <sridr8@gmail.com>
2022-11-09 17:48:11 +0530

    fetching individual post on input value (id)

--- react-hooks/src/components/DataFetching.js
+++ react-hooks/src/components/DataFetching.js
@@ -2,20 +2,23 @@ import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 
 function DataFetching() {
-  const [posts, setPosts] = useState([])
+  const [post, setPost] = useState({})
+  const [id, setId] = useState(1)
   useEffect(() => {
-    axios.get('http://jsonplaceholder.typicode.com/posts')
+    axios.get(`http://jsonplaceholder.typicode.com/posts/${id}`)
     .then(res => {
       console.log(res)
-      setPosts(res.data)
+      setPost(res.data)
     })
     .catch(error => console.log(error))  
-  },[]) // [] <- dependency list
+  },[id]) // [] <- dependency list
   return (
     <div>
-      <ol>
+      <input type='text' value={id} onChange={e => setId(e.target.value)}/>
+      <div>{post.title}</div>
+      {/* <ol>
         {posts.map(post => (<li key={post.id}>{post.title}</li>))}
-      </ol>
+      </ol> */}
     </div>
   )
 }
