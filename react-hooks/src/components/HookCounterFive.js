60bc3d3e1736f60e8cfd0b0a0e45f74ce1ca9e92
Sridhar Kothapalli <sridr8@gmail.com>
2022-11-09 11:53:04 +0530

    conditionally run effects

--- react-hooks/src/components/HookCounterFive.js
+++ react-hooks/src/components/HookCounterFive.js
@@ -6,7 +6,7 @@ function HookCounterFive() {
   useEffect(() => {
     console.log('useEffect - Updating document title')
     document.title = `Clicked ${count} times`
-  })
+  },[count])
   return (
     <div>
       <input type='text' value={name} onChange={e => setName(e.target.value)}/>
