2bef20aeb63d25fe4422c859182b01112490c798
Sridhar Kothapalli <sridr8@gmail.com>
2022-11-09 11:00:28 +0530

    useEffect hook

--- react-hooks/src/components/HookCounterFive.js
+++ react-hooks/src/components/HookCounterFive.js
@@ -2,6 +2,9 @@ import React, {useState, useEffect} from 'react'
 
 function HookCounterFive() {
   const [count, setCount] = useState(0)
+  useEffect(() => {
+    document.title = `Clicked ${count} times`
+  })
   return (
     <div>
       <button onClick={() => setCount(count + 1)}>Clicked {count} times</button>
