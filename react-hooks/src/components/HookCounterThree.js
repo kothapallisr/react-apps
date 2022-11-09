a7adcf5d93073f8c144634fc72b3d4b62a668c43
Sridhar Kothapalli <sridr8@gmail.com>
2022-11-09 10:01:10 +0530

    merging object while using useState

--- react-hooks/src/components/HookCounterThree.js
+++ react-hooks/src/components/HookCounterThree.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function HookCounterThree() {
   const [name, setName] = useState({firstName: '', lastName: ''})
   return (
     <form>
-      <input type='text' value={name.firstName} />
-      <input type='text' value={name.lastName} />
+      <input type='text' value={name.firstName} onChange={e => setName({...name, firstName: e.target.value})}/>
+      <input type='text' value={name.lastName} onChange={e => setName({...name, lastName: e.target.value})}/>
       <h3>Your first name is: {name.firstName}</h3>
       <h3>Your last name is: {name.lastName}</h3>
     </form>
