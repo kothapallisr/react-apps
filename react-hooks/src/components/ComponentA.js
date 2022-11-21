15615021bfc4552f8c2619ee63efb6c99697841d
Sridhar Kothapalli <sridr8@gmail.com>
2022-11-21 10:38:23 +0530

    accessing state using useContext

--- react-hooks/src/components/ComponentA.js
+++ react-hooks/src/components/ComponentA.js
@@ -4,7 +4,7 @@ import {CountContext} from '../App'
 function ComponentA() {
   const countContext = useContext(CountContext)
   return (
-    <div>ComponentA
+    <div>ComponentA: {countContext.countState}
       <button onClick={() => countContext.countDispatch('increment')}>Increment</button>
       <button onClick={() => countContext.countDispatch('decrement')}>Decrement</button>
       <button onClick={() => countContext.countDispatch('reset')}>Reset</button>
