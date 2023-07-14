db78b673fce16f55419500dc93c22c4e0f5e41b1
Sridhar Kothapalli <sridr8@gmail.com>
2023-07-14 12:20:08 +0530

    cleanup

--- hello/src/components/Person.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Person({person}) {
-  return (
-    <div><h2>I'm {person.name} of age {person.age} and know {person.skill}</h2></div>
-  )
-}
-
-export default Person
--- notes-app/src/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-function Footer() {
-  var currentYear = new Date().getFullYear();
-  return <footer><p>Copyright © {currentYear}</p></footer>;
-}
-
-export default Footer;
